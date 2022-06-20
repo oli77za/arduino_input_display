31f636310fefe3b418bbbf13a19bcaac725f39a0
Oliver Zampieri <oliver.zampieri@gmail.com>
2022-06-20 10:50:04 +0100

    Refactor input selection

--- include/input_display.h
+++ include/input_display.h
@@ -1,7 +1,6 @@
 #ifndef __INPUT_DISPLAY_H__
 #define __INPUT_DISPLAY_H__
 
-
 #include <LiquidCrystal_I2C.h> // Version 1.1.2
 #include <Arduino.h>
 
@@ -12,12 +11,14 @@ const char *LCD_STRING_CH3 = "    NOT USED";
 const char *LCD_STRING_CH4 = "     CD/DVD ";
 const char *LCD_STRING_CH5 = "   COMPUTER ";
 
+const char *LCD_STRINGS[9];
+
 const int ch_2 = A0; // Inputs from HDMI switcher
 const int ch_3 = A1;
 const int ch_4 = A2;
 const int ch_5 = A3;
 
-int curr_source = 0;       // Used to store which source is currently displayed
+int curr_source = 0; // Used to store which source is currently displayed
 
 void check_source(void);
 
--- src/input_display.cpp
+++ src/input_display.cpp
@@ -4,17 +4,27 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 or )x20 for a
 
 void setup()
 {
+  LCD_STRINGS[0] = LCD_STRING_CH1;
+  LCD_STRINGS[1] = LCD_STRING_CH2;
+  LCD_STRINGS[2] = LCD_STRING_CH3;
+  LCD_STRINGS[4] = LCD_STRING_CH4;
+  LCD_STRINGS[8] = LCD_STRING_CH5;
+
   pinMode(ch_2, INPUT);
   pinMode(ch_3, INPUT);
   pinMode(ch_4, INPUT);
   pinMode(ch_5, INPUT);
-  Serial.begin(9600);
 
   lcd.init();      // Initialize the lcd
   lcd.backlight(); // Turn ON LCD backlight
   lcd.clear();
 
   lcd.print(LCD_STRING_SOURCE);
+
+  #ifdef DEBUG
+  Serial.begin(9600);
+  #endif
+
 }
 
 void loop()
@@ -30,38 +40,14 @@ void check_source(void)
   bool ch4_in = digitalRead(ch_4) == HIGH;
   bool ch5_in = digitalRead(ch_5) == HIGH;
 
+  byte source = 0 |ch2_in |(ch3_in << 1) |(ch4_in << 2) |(ch5_in << 3); 
   
-  if (!ch2_in && !ch3_in && !ch4_in && !ch4_in && curr_source != 1)
-  {
-    curr_source = 1;
+  if (source != curr_source) {
+    curr_source = source;
     lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH1); // Channel 1 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH1);
-  }
-  else if (ch2_in && curr_source != 2)
-  {
-    curr_source = 2;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH2); // Channel 2 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH2);
-  }
-  else if (ch3_in && curr_source != 3)
-  {
-    curr_source = 3;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH3); // Channel 3 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH3);
-  }
-  else if (ch4_in && curr_source != 4)
-  {
-    curr_source = 4;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH4); // Channel 4 of HDMI switcher LCD name
-  }
-  else if (ch5_in && curr_source != 5)
-  {
-    curr_source = 5;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH5); // Channel 5 of HDMI switcher LCD name
+    lcd.print(LCD_STRINGS[source]); // Channel 1 of HDMI switcher LCD name
+    #ifdef DEBUG
+    Serial.write(LCD_STRINGS[source]);
+    #endif
   }
 }
