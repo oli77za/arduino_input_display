7d64b9c439dc1059de6dc14a759aca36a2dd84d8
Oliver Zampieri <oliver.zampieri@gmail.com>
2022-06-28 12:45:34 +0100

    Invert logic. Led ON reads LOW

--- include/input_display.h
+++ include/input_display.h
@@ -1,24 +1,22 @@
 #ifndef __INPUT_DISPLAY_H__
 #define __INPUT_DISPLAY_H__
 
-
 #include <LiquidCrystal_I2C.h> // Version 1.1.2
 #include <Arduino.h>
 
 const char *LCD_STRING_SOURCE = "**** SOURCE ****";
-const char *LCD_STRING_CH1 = "   AV INPUT ";
-const char *LCD_STRING_CH2 = "      PS_4  ";
-const char *LCD_STRING_CH3 = "    NOT USED";
-const char *LCD_STRING_CH4 = "     CD/DVD ";
-const char *LCD_STRING_CH5 = "   COMPUTER ";
+const char *LCD_STRING_0 = "    NO INPUT";
+const char *LCD_STRING_1 = "   AV INPUT ";
+const char *LCD_STRING_2 = "      PS_4  ";
+const char *LCD_STRING_3 = "    NOT USED";
+const char *LCD_STRING_4 = "     CD/DVD ";
+const char *LCD_STRING_5 = "   COMPUTER ";
 
-const int ch_2 = A0; // Inputs from HDMI switcher
-const int ch_3 = A1;
-const int ch_4 = A2;
-const int ch_5 = A3;
+const char *LCD_STRINGS[6];
 
-int curr_source = 0;       // Used to store which source is currently displayed
+int curr_source = -1; // Used to store which source is currently displayed
 
-void check_source(void);
+int check_source(void);
+void display_output(int source);
 
 #endif
--- src/input_display.cpp
+++ src/input_display.cpp
@@ -4,64 +4,68 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 or )x20 for a
 
 void setup()
 {
-  pinMode(ch_2, INPUT);
-  pinMode(ch_3, INPUT);
-  pinMode(ch_4, INPUT);
-  pinMode(ch_5, INPUT);
-  Serial.begin(9600);
+  LCD_STRINGS[0] = LCD_STRING_0;
+  LCD_STRINGS[1] = LCD_STRING_1;
+  LCD_STRINGS[2] = LCD_STRING_2;
+  LCD_STRINGS[3] = LCD_STRING_3;
+  LCD_STRINGS[4] = LCD_STRING_4;
+  LCD_STRINGS[5] = LCD_STRING_5;
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A6, INPUT);
 
   lcd.init();      // Initialize the lcd
   lcd.backlight(); // Turn ON LCD backlight
   lcd.clear();
 
   lcd.print(LCD_STRING_SOURCE);
+
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
 }
 
 void loop()
 {
-  check_source();
+  const int source = check_source();
+  display_output(source);
   delay(10);
 }
 
-void check_source(void)
+int check_source(void)
 {
-  bool ch2_in = digitalRead(ch_2) == HIGH;
-  bool ch3_in = digitalRead(ch_3) == HIGH;
-  bool ch4_in = digitalRead(ch_4) == HIGH;
-  bool ch5_in = digitalRead(ch_5) == HIGH;
+  int source = 0;
+  if (digitalRead(A0) == LOW) {
+    source = 1;
+  }
+  if (digitalRead(A1) == LOW) {
+    source = 2;
+  }
+  if (digitalRead(A2) == LOW) {
+    source = 3;
+  }
+  if (digitalRead(A3) == LOW) {
+    source = 4;
+  }
+  if (digitalRead(A6) == LOW) {
+    source = 5;
+  }
 
-  
-  if (!ch2_in && !ch3_in && !ch4_in && !ch4_in && curr_source != 1)
+  return source;
+}
+
+void display_output(int source)
+{
+  if (source != curr_source)
   {
-    curr_source = 1;
+    curr_source = source;
     lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH1); // Channel 1 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH1);
-  }
-  else if (ch2_in && curr_source != 2)
-  {
-    curr_source = 2;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH2); // Channel 2 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH2);
-  }
-  else if (ch3_in && curr_source != 3)
-  {
-    curr_source = 3;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH3); // Channel 3 of HDMI switcher LCD name
-    Serial.write(LCD_STRING_CH3);
-  }
-  else if (ch4_in && curr_source != 4)
-  {
-    curr_source = 4;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH4); // Channel 4 of HDMI switcher LCD name
-  }
-  else if (ch5_in && curr_source != 5)
-  {
-    curr_source = 5;
-    lcd.setCursor(0, 1);
-    lcd.print(LCD_STRING_CH5); // Channel 5 of HDMI switcher LCD name
+    lcd.print(LCD_STRINGS[source]); // Channel 1 of HDMI switcher LCD name
+#ifdef DEBUG
+    Serial.write(LCD_STRINGS[source]);
+#endif
   }
 }
